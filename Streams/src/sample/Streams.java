f53f03c050bc0d5d36c89438a32e11856b3c5494
CiSigep <Cisigep@gmail.com>
2018-05-30 11:26:44 -0400

    Add Flatmap

--- Streams/src/sample/Streams.java
+++ Streams/src/sample/Streams.java
@@ -1,7 +1,9 @@
 package sample;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Streams {
 	
@@ -11,7 +13,38 @@ public class Streams {
                 "Elapsam semel occasionem non ipse potest Iuppiter reprehendere.",
                 "Plaudite. Acta est fabula.");
 		
-		lines.stream().filter(s -> s.length() > 15).forEach(System.out::println);
+		lines.stream().map(String::toUpperCase).filter(s -> s.length() > 15).forEach(System.out::println);
+		
+		Stream<String> phrases = Stream.of("Tu Fui, Ego Eris.", "Memento Mori.");
+		Stream<String> oPhrases = Stream.of("Elapsam semel occasionem non ipse potest Iuppiter reprehendere.",
+                "Plaudite. Acta est fabula.");
+		
+		Stream<String> concatStream = Stream.concat(phrases, oPhrases);
+		
+		System.out.println(concatStream.count());
+		
+		Employee e1 = new Employee("John", 25);
+		Employee e2 = new Employee("James", 30);
+		Employee e3 = new Employee("Jack", 40);
+		Employee e4 = new Employee("Josh", 50);
+		
+		Department d = new Department("Human Resources");
+		
+		d.addEmployee(e1);
+		d.addEmployee(e2);
+		d.addEmployee(e3);
+		
+		Department d2 = new Department("Accounting");
+		d2.addEmployee(e4);
+		
+		List<Department> departments = new ArrayList<Department>();
+		
+		departments.add(d);
+		departments.add(d2);
+		
+		departments.stream().flatMap(department -> department.getEmployees().stream()).forEach(System.out::println);
+		
+		
 	}
 
 }
